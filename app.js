07ac8b9e88df85727e796dc99b14e22d7a0ec1da
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-06-27 20:26:17 +0200

    presets

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.get('/', (req, res) => {
+  res.send('all good');
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
+
